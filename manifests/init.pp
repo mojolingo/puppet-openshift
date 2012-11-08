806e6a0edff7441fd35d1c8997a6fd03b8f66dd6
Ben Klang <bklang@mojolingo.com>
2012-11-07 20:23:29 -0500

    Begin work on manifest

---
+++ manifests/init.pp
@@ -0,0 +1,24 @@
+class openshift(
+  $openshift_domain = 'openshift.local',
+) {
+
+  class { ntp:
+    ensure     => running,
+    servers    => [ "time.apple.com iburst",
+                    "pool.ntp.org iburst",
+                    "clock.redhat.com iburst"],
+    autoupdate => true,
+  }
+
+  yumrepo { “openshift”:
+    mirrorlist => ‘https://mirror.openshift.com/pub/origin-server/nightly/enterprise/2012-10-18/Infrastructure/x86_64/os/',
+    enabled => 1,
+    gpgcheck => 0,
+  }
+
+  package { [bind, bind-utils]:
+    require => Yumrepo[openshift],
+    ensure => present,
+  }
+
+}
