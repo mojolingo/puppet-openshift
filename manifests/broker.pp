12827fbad972c0f542e82923a94cfdcbe8d79319
Ben Klang <bklang@mojolingo.com>
2012-11-08 21:04:59 -0500

    Work on named configuration for Broker

--- manifests/broker.pp
+++ manifests/broker.pp
@@ -2,7 +2,7 @@ class openshift::broker(
   $domain => 'openshift.local',
   $password => 'marionnette',
 ) {
-  package { [bind, bind-utils, mcollective-client, httpd]:
+  package { [bind, bind-utils, mcollective-client, httpd, policycoreutils]:
     require => Yumrepo[openshift],
     ensure => present,
   }
@@ -15,8 +15,69 @@ class openshift::broker(
     unless => "[ -f /var/named/K${domain}*.private ]",
   }
 
+  service { "named":
+    ensure => running
+    require => Exec["named restorecon"],
+  }
+
+  exec { "named restorecon":
+    command => "/sbin/restorecon -rv /etc/rndc.* /etc/named.* /var/named",
+    require => [
+      File["/etc/rndc.key"],
+      File["/var/named/forwarders.conf"],
+      File["/etc/named.conf"],
+      File["dynamic zone"],
+    ],
+  }
+
   file { "/etc/rndc.key":
-    owner => root, group => named, mode => 0640
+    owner => root, group => named, mode => 0640,
+    require => Exec["create rndc.key"],
+  }
+
+  exec { "create rndc.key":
+    command => "/usr/sbin/rndc-confgen -a -r /dev/urandom",
+    unless => "[ -f /etc/rndc.key ]",
+  }
+
+  file { "/var/named/forwarders.conf":
+    owner => root, group => named, mode => 0640,
+    content => "forwarders { 8.8.8.8; 8.8.4.4; };\n"
+  }
+
+  file { "/var/named":
+    ensure => directory,
+    owner => named, group => named, mode => 0755,
+  }
+
+  file { "/var/named/dynamic":
+    ensure => directory,
+    owner => named, group => named, mode => 0755
+    require => File["/var/named"],
+  }
+
+  file { "dynamic zone":
+    path => "/var/named/dynamic/${domain}.db",
+    content => template("files/dynamic-zone.db.erb"),
+    owner => named, group => named, mode => 0644,
+    require => File["/var/named"],
+  }
+
+  file { "named key":
+    path => "/var/named/${domain}.key",
+    content => "
+key ${domain} {
+  algorithm HMAC-MD5;
+  secret "${KEY}";
+};
+",
+    owner => named, group => named, mode => 0444,
+    require => File["/var/named"],
+  }
+
+  file { "/etc/named.conf":
+    owner => root, group => named, mode => 0644,
+    content => template("named.conf.erb"),
   }
 
   #
